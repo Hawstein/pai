4be141243d477e860c027201dd6f2d899df8220a
jiluo <weiqiyiji@gmail.com>
2014-02-08 13:07:20 +0800

    FIx

--- server.cpp
+++ server.cpp
@@ -10,6 +10,7 @@ using namespace hackday;
 void usage(char *prog);
 
 static SURFFeatureManager manager;
+static std::vector<Data> dataset;
 
 static int MatchHandler(struct mg_connection *conn) {
   if (strcmp(conn->uri, "/match") == 0) {
@@ -21,9 +22,8 @@ static int MatchHandler(struct mg_connection *conn) {
       mg_send_status(conn, 400);
       return MG_REQUEST_PROCESSED;
     }
-    const string data_name = "data.hd";
-    std::vector<Data> dataset;
-    Filter::LoadData(data_name, dataset);
+
+
     std::map<std::string, std::string> candidates =
       Filter::GetCandidates(filepath, dataset);
     if (candidates.empty()) {
@@ -52,6 +52,7 @@ static int MatchHandler(struct mg_connection *conn) {
       mg_printf_data(conn, "");
     }
   }
+  printf("Process complete\n");
   return MG_REQUEST_PROCESSED;
 }
 
@@ -61,6 +62,8 @@ int main(int argc, char **argv) {
   Timer timer;
   timer.Start();
   //manager.CalculateFeatureSet(kVideo, "/Users/jiluo/projects/movie");
+  const string data_name = "data.hd";
+  Filter::LoadData(data_name, dataset);
   manager.LoadFeatureSet();
   printf("Load time elapsed: %ld\n", timer.elapsed_millis());
   StartHTTPServer(&MatchHandler);
